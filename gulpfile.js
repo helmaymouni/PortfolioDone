74aa845d5a6e3cc85f90b8148a2706546b4a7b5b
Helmaymouni <hasnae.elmaymouni@gmail.com>
2021-01-29 13:57:47 +0100

    add fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -57,11 +57,7 @@ function webFontTask() {
 
 function cssawesomeTask(){
   return src('fontawesome/css/**/*.css')
-  .pipe(sourcemaps.init())
-  .pipe(concat('style.css'))
-  .pipe(postcss([autoprefixer(),cssnano()]))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('dist/fontawesome/css'));
+    .pipe(gulp.dest('dist/fontawesome/css'));
   }
 
 
